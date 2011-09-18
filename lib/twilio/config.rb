86f4f9e581dcced5502a6209ff865d22313ab578
Stevie Graham <sjtgraham@mac.com>
2011-09-18 14:21:34 +0100

    fix typo in config deprecation warning

--- lib/twilio/config.rb
+++ lib/twilio/config.rb
@@ -3,7 +3,7 @@ module Twilio
     def setup(opts={}, &blk)
       if block_given?
         instance_eval &blk
-        warn 'The block syntax for configuration is deprecated. Use an options hash instead, e.g. Twilio::Config.create account_sid: "AC00000000000000000000000", auth_token: "xxxxxxxxxxxxxxxxxxx"'
+        warn 'The block syntax for configuration is deprecated. Use an options hash instead, e.g. Twilio::Config.setup account_sid: "AC00000000000000000000000", auth_token: "xxxxxxxxxxxxxxxxxxx"'
       else
         opts.map do |k,v|
           const = k.to_s.upcase
