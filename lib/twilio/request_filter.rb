28ee7dd14bfd843f88f9b64a564bb86ad621566d
Daniel Insley <dan@pulsesoftware.ca>
2011-11-03 15:16:09 -0700

    fixed request filter to work with rails 3.1

--- lib/twilio/request_filter.rb
+++ lib/twilio/request_filter.rb
@@ -6,16 +6,16 @@ module Twilio
   module RequestFilter
     def filter(controller)
       request = controller.request
-      if request.format.voice? && request.ssl?
-        controller.head(:forbidden) if expected_signature_for(request) != request.env['X-Twilio-Signature']
+      if request.format.try(:voice?) && request.ssl?
+        controller.head(:forbidden) if expected_signature_for(request) != request.env['HTTP_X_TWILIO_SIGNATURE']
       end
     end
 
     private
     def expected_signature_for(request)
-      string_to_sign = request.url + request.params.sort.join
-      digest         = OpenSSL::Digest::Digest.new 'sha1'
-      hash           = OpenSSL::HMAC.digest digest, Twilio::AUTH_TOKEN, string_to_sign
+      string_to_sign = request.url + request.request_parameters.sort.join
+      digest         = OpenSSL::Digest::Digest.new('sha1')
+      hash           = OpenSSL::HMAC.digest(digest, Twilio::AUTH_TOKEN, string_to_sign)
 
       Base64.encode64(hash).strip
     end
