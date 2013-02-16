f7231b740b27092bfc32cd57c24fa87f6841d519
Alex Sharp <ajsharp@gmail.com>
2013-02-15 19:13:11 -0800

    change Twilio::Config internals to module attrs
    Fixes #26

--- lib/twilio/config.rb
+++ lib/twilio/config.rb
@@ -1,13 +1,16 @@
+require 'active_support/core_ext/module'
+
 module Twilio
   module Config
+    mattr_accessor :account_sid, :auth_token
+
     def setup(opts={}, &blk)
       if block_given?
         instance_eval &blk
         warn 'The block syntax for configuration is deprecated. Use an options hash instead, e.g. Twilio::Config.setup account_sid: "AC00000000000000000000000", auth_token: "xxxxxxxxxxxxxxxxxxx"'
       else
         opts.map do |k,v|
-          const = k.to_s.upcase
-          Twilio.const_set(const, v) unless Twilio.const_defined? const
+          send("#{k}=", v)
         end
       end
 
